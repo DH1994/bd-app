c2cea57769eaf151bb0a8e1e25794b6078e519af
D.Hergaarden <dhergaarden@dhebook.lan>
2020-03-11 22:20:54 +0100

    Random color

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const os = require('os')
 
 var ifaces = os.networkInterfaces();
 var ips = []
+var color = getRandomColor()
+
 for(dev in ifaces)
 {
   ips.push(ifaces[dev][0].address)
@@ -16,8 +18,17 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   // OLD CODE
   //res.send('Hello World!')
-  res.render('index', {ips: ips});
+  res.render('index', {ips: ips, color: color});
 })
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
